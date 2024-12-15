46ef4401111e1c0c51d7b72234741e34e66adeef
margulis <wfptny@gmail.com>
2024-12-15 20:39:21 +0100

    first commit

--- src/LambdaThree.java
+++ src/LambdaThree.java
@@ -1,14 +1,8 @@
 import java.util.stream.Stream;
-public class LambdaThree {
-    public Stream<Double> generateLCG(double a, double c, double m) {
-        return Stream.iterate(0d, d ->  ((a * d + c) % m));
-    }
-}
-class LambdaThreeSandBox {
     public static void main(String[] args) {
         double a = 25214903917d;
         double c = 11d;
         double m = Math.pow(2, 48);
-        new LambdaThree().generateLCG(a, c, m).forEach(System.out::println);
+        Stream.iterate(0d, d ->  ((a * d + c) % m))//Get infinity stream of pseudorandom numbers
+                .forEach(System.out::println);//Just printing it to console
     }
-}
